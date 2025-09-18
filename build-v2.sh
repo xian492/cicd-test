94e9ca1d4f3476ab0a4296d7726d2762f5e44c27
xian492 <xianxx492@gmail.com>
2025-09-18 15:09:05 +0800

    update

---
+++ build-v2.sh
@@ -0,0 +1,3 @@
+docker build -t xian492/cicd-demo-ws4:v2 .
+
+docker push xian492/cicd-demo-ws4:v2
---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t xian492/cicd-demo-ws4:v1 .
+
+docker push xian492/cicd-demo-ws4:v1
